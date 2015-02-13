5c0241e22792f78f7f83131a43dd9cb0ddce0993
Ken <krrgithub@gmail.com>
2015-02-13 13:44:29 -0700

    Added pdf and image files.

--- snippets/py_to_png.pl
+++ snippets/py_to_png.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-for my $x (1..23) {
+for my $x (1..24) {
     system("rm $x.png");
-    system("pygmentize -f png -l python -O style=pastie,font_name='Source Code Pro Semibold',font_size=16,line_numbers=False -o $x.png $x.py");    
+    system("pygmentize -f png -l python -O style=vs,font_name='Source Code Pro Semibold',font_size=12,line_numbers=False -o $x.png $x.py");    
 }
